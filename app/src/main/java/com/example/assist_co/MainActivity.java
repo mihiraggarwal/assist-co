96f04f3a1c8af63f9d48df5ac008c0da57d3e329
Mihir Aggarwal <mihir.aggarwal1000@gmail.com>
2020-06-09 00:30:26 +0530

    companies sort

--- app/src/main/java/com/example/assist_co/MainActivity.java
+++ app/src/main/java/com/example/assist_co/MainActivity.java
@@ -18,11 +18,21 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+
+import static com.google.common.collect.Iterables.size;
 
 public class MainActivity extends AppCompatActivity implements CompaniesRecyclerAdapter.OnCompanyListener {
     private FirebaseAuth mAuth;
@@ -30,6 +40,14 @@ public class MainActivity extends AppCompatActivity implements CompaniesRecycler
     private ArrayList<Company> mCompanies = new ArrayList<>();
     private CompaniesRecyclerAdapter mCompaniesRecyclerAdapter;
     private static final String TAG = "MainActivity";
+
+    private List<String> mCompare = new ArrayList<>();
+    private ArrayList<String> mDuplicate = new ArrayList<>();
+    private List<String> mToCompare = new ArrayList<>();
+
+    private Dictionary <Integer, String> similarities = new Hashtable <Integer, String>();
+    private ArrayList lengths = new ArrayList();
+    private ArrayList docIds = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +57,37 @@ public class MainActivity extends AppCompatActivity implements CompaniesRecycler
         mRecyclerView = findViewById(R.id.companies_recyclerView);
         mAuth = FirebaseAuth.getInstance();
 
-        initRecyclerView();
-        insertCompanies();
+        getUser();
+
     }
 
-    private void insertCompanies() {
+    private void getUser() {
+        Log.d(TAG, "getUser: running");
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        db.collection("Users")
+                .whereEqualTo("uid", user.getUid())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+                                String [] splitter = documentSnapshot.getString("services_required").split(", ");
+                                mCompare = Arrays.asList(splitter);
+                                Log.d(TAG, "onComplet: " + mCompare);
+
+                                initRecyclerView();
+                                findCompanies();
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void findCompanies() {
+        Log.d(TAG, "insertCompanies: running");
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
         final FirebaseUser user = mAuth.getCurrentUser();
         db.collection("Users")
                 .get()
@@ -54,27 +97,70 @@ public class MainActivity extends AppCompatActivity implements CompaniesRecycler
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
                                 Log.d(TAG, "onComplete: task successful");
-                                if (documentSnapshot.getString("uid").equals(user.getUid())){
-                                    Log.d(TAG, "onComplete: " + "equal");
-                                }
-                                else {
-                                    Company company = new Company();
-                                    company.setName(documentSnapshot.getString("name"));
-                                    company.setPhone(documentSnapshot.getString("phone"));
-                                    company.setWebsite(documentSnapshot.getString("website"));
-                                    company.setYear(documentSnapshot.getString("year_of_creation"));
-                                    company.setOverview(documentSnapshot.getString("overview"));
-                                    company.setServicesProvided("Services provided -> " + documentSnapshot.getString("services_provided"));
-                                    company.setServicesRequired(documentSnapshot.getString("services_required"));
-                                    mCompanies.add(company);
+                                if (documentSnapshot.getString("uid").equals(user.getUid())) {
+                                    // continue
+                                } else {
+                                    String[] splitter_other = documentSnapshot.getString("services_provided").split(",");
+                                    mToCompare = Arrays.asList(splitter_other);
+                                    Log.d(TAG, "onComplete: " + mToCompare);
+                                    List<String> checker = new ArrayList<>();
+                                    for (String i : mToCompare) {
+                                        if (mCompare.contains(i)) {
+                                            checker.add(i);
+                                        }
+                                    }
+                                    similarities.put(size(checker), documentSnapshot.getString("docId"));
                                 }
                             }
-                            mCompaniesRecyclerAdapter.notifyDataSetChanged();
+                            insertCompanies();
+
                         } else {
                             Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
+    }
+
+    private void insertCompanies() {
+        for (Enumeration i = similarities.keys(); i.hasMoreElements();){
+            lengths.add(i.nextElement());
+        }
+        Collections.sort(lengths, Collections.reverseOrder());
+        Log.d(TAG, "onComplete: " + lengths);
+        for (Object i : lengths){
+            docIds.add(similarities.get(i));
+        }
+        Log.d(TAG, "onComplete: " + similarities);
+        Log.d(TAG, "onComplete: " + docIds);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        for (Object i : docIds){
+            Log.d(TAG, "insertCompanies: " + i.toString());
+            db.collection("Users")
+                    .document(i.toString())
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful()){
+                                Log.d(TAG, "onComplete: success");
+                                DocumentSnapshot documentSnapshot = task.getResult();
+                                Company company = new Company();
+                                company.setName(documentSnapshot.getString("name"));
+                                company.setPhone(documentSnapshot.getString("phone"));
+                                company.setWebsite(documentSnapshot.getString("website"));
+                                company.setYear(documentSnapshot.getString("year_of_creation"));
+                                company.setOverview(documentSnapshot.getString("overview"));
+                                company.setServicesProvided("Services provided -> " + documentSnapshot.getString("services_provided"));
+                                company.setServicesRequired(documentSnapshot.getString("services_required"));
+                                mCompanies.add(company);
+
+                                mCompaniesRecyclerAdapter.notifyDataSetChanged();
+                            }
+                        }
+                    });
+
+        }
     }
 
     private void initRecyclerView() {
